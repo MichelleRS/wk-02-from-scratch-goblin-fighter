bf666fa08da1da6396f11e15099a89899c00f3b2
Michelle Ross <miross629@gmail.com>
2022-11-08 09:29:21 -0800

    Get DOM elements

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 /* Imports */
 
 /* Get DOM Elements */
+const goblinsListEl = document.querySelector('.goblins');
+const formEl = document.querySelector('form');
+const playerHPEl = document.querySelector('#player-hp');
+const defeatedNumberEl = document.querySelector('#defeated-number');
+const playerImg = document.querySelector('#player-img');
 
 /* State */
 
