1c9b154c40050ec3294a3edbaf6c97132080754f
Michelle Ross <miross629@gmail.com>
2022-11-08 09:39:03 -0800

    Create goblins object

--- app.js
+++ app.js
@@ -8,6 +8,27 @@ const defeatedNumberEl = document.querySelector('#defeated-number');
 const playerImg = document.querySelector('#player-img');
 
 /* State */
+const goblins = [
+    {
+        id: 1,
+        name: 'Mrs. Goblin',
+        hp: 2,
+    },
+    {
+        id: 2,
+        name: 'Beau',
+        hp: 5,
+    },
+    {
+        id: 3,
+        name: 'Stinky',
+        hp: 3,
+    },
+];
+
+let currentId = 4;
+let playerHP = 10;
+let defeatedCount = 0;
 
 /* Events */
 
