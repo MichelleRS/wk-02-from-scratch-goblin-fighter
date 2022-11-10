d3a1e8fa4e72b9a2db7246b5d570c588694a91d9
Michelle Ross <miross629@gmail.com>
2022-11-10 15:05:53 -0800

    Add goblinEl-style to rendered goblins

--- render-utils.js
+++ render-utils.js
@@ -8,14 +8,17 @@ export function renderGoblins(goblinData) {
 
     // name
     nameEl.textContent = goblinData.name;
+    nameEl.classList.add('goblinEl-style');
 
     // emoji
     emojiEl.id = `goblin-face-${goblinData.id}`;
     emojiEl.textContent = goblinData.hp > 0 ? '😈' : '💀';
+    emojiEl.classList.add('goblinEl-style');
 
     // hp
     hpEl.id = `goblin-hp-${goblinData.id}`;
     hpEl.textContent = goblinData.hp < 0 ? 0 : goblinData.hp;
+    hpEl.classList.add('goblinEl-style');
 
     if (goblinData.hp < 0) {
         goblinEl.classList.add('defeated');
